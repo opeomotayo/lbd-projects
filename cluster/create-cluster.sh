474d7621a79e49217a3cd0572c80518dcf164922
Ope Omotayo <opeomotayo@gmail.com>
2023-08-03 20:39:13 +0000

    pushing logging code

--- cluster/create-cluster.sh
+++
@@ -1,5 +0,0 @@
- curl -sfL https://get.k3s.io | sh -
- mkdir -p $HOME/.kube
- sudo cp /etc/rancher/k3s/k3s.* $HOME/.kube/config
- sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo cat /var/lib/rancher/k3s/server/node-token
